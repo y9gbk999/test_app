3fec74886779483f210c33320680da8c5e03a407
やこ <mac@yakonoMacBook-puro.local>
2021-02-26 23:42:49 +0900

    add new file

--- test_app/lib/main.dart
+++ test_app/lib/main.dart
@@ -1,4 +1,11 @@
+//import 'dart:html';
+
 import 'package:flutter/material.dart';
+
+// image_picker
+import 'package:image_picker/image_picker.dart';
+// File クラスを使用するのでこれもインポートします
+import 'dart:io';
 
 void main() {
   runApp(MyApp());
@@ -51,6 +58,20 @@ class MyHomePage extends StatefulWidget {
 
 class _MyHomePageState extends State<MyHomePage> {
   int _counter = 0;
+  File _image;
+  final picker = ImagePicker();
+
+  //画像の読み込み
+  Future _getImage()async{
+    //final PickedFile = await Picker.getImage(source:ImageSource.camera);  //カメラ
+    final pickedFile = await picker.getImage(source: ImageSource.gallery);  //カメラロール
+
+    setState(() {
+      if(PickedFile != null){
+        _image = File(pickedFile.path);
+      }
+    });
+  }
 
   void _incrementCounter() {
     setState(() {
@@ -71,57 +92,67 @@ class _MyHomePageState extends State<MyHomePage> {
     // The Flutter framework has been optimized to make rerunning build methods
     // fast, so that you can just rebuild anything that needs updating rather
     // than having to individually change instances of widgets.
-    return Scaffold(
-      appBar: AppBar(
-        // Here we take the value from the MyHomePage object that was created by
-        // the App.build method, and use it to set our appbar title.
-        title: Text(widget.title),
-         backgroundColor: Colors.blue,
-      ),
-      body: Center(
-        // Center is a layout widget. It takes a single child and positions it
-        // in the middle of the parent.
-        child: Column(
-          // Column is also a layout widget. It takes a list of children and
-          // arranges them vertically. By default, it sizes itself to fit its
-          // children horizontally, and tries to be as tall as its parent.
-          //
-          // Invoke "debug painting" (press "p" in the console, choose the
-          // "Toggle Debug Paint" action from the Flutter Inspector in Android
-          // Studio, or the "Toggle Debug Paint" command in Visual Studio Code)
-          // to see the wireframe for each widget.
-          //
-          // Column has various properties to control how it sizes itself and
-          // how it positions its children. Here we use mainAxisAlignment to
-          // center the children vertically; the main axis here is the vertical
-          // axis because Columns are vertical (the cross axis would be
-          // horizontal).
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: <Widget>[
-            Text(
-              'Hello Flutter',
+    var scaffold = Scaffold(
+          appBar: AppBar(
+            // Here we take the value from the MyHomePage object that was created by
+            // the App.build method, and use it to set our appbar title.
+            title: Text(widget.title),
+             backgroundColor: Colors.blue,
+          ),
+          body: Center(
+            // Center is a layout widget. It takes a single child and positions it
+            // in the middle of the parent.
+            child: Column(
+              // Column is also a layout widget. It takes a list of children and
+              // arranges them vertically. By default, it sizes itself to fit its
+              // children horizontally, and tries to be as tall as its parent.
+              //
+              // Invoke "debug painting" (press "p" in the console, choose the
+              // "Toggle Debug Paint" action from the Flutter Inspector in Android
+              // Studio, or the "Toggle Debug Paint" command in Visual Studio Code)
+              // to see the wireframe for each widget.
+              //
+              // Column has various properties to control how it sizes itself and
+              // how it positions its children. Here we use mainAxisAlignment to
+              // center the children vertically; the main axis here is the vertical
+              // axis because Columns are vertical (the cross axis would be
+              // horizontal).
+              mainAxisAlignment: MainAxisAlignment.center,
+              children: <Widget>[
+                _image == null
+                  ? Text('No image selected.')
+                  :Image.file(_image),
+                Text(
+                  'Hello Flutter',
+                ),
+                Text(
+                  'You have pushed the button this many times:',
+                ),
+                Text(
+                  '$_counter',
+                  style: Theme.of(context).textTheme.headline4,
+                ),
+              ],
             ),
-            Text(
-              'You have pushed the button this many times:',
-            ),
-            Text(
-              '$_counter',
-              style: Theme.of(context).textTheme.headline4,
-            ),
-          ],
-        ),
-      ),
-      backgroundColor: Colors.red[300],
-      //body: Right(
-        //child: column(
-          //Text(
-            //'Hello World'),
-
-      floatingActionButton: FloatingActionButton(
-        onPressed: _incrementCounter,
-        tooltip: 'Increment',
-        child: Icon(Icons.add),
-      ), // This trailing comma makes auto-formatting nicer for build methods.
-    );
+          ),
+          backgroundColor: Colors.red[300],
+          //body: Right(
+            //child: column(
+              //Text(
+                //'Hello World'),
+    
+          //floatingActionButton: FloatingActionButton(
+          //  onPressed: _incrementCounter,
+          //  tooltip: 'Increment',
+          //  child: Icon(Icons.add),
+          //), // This trailing comma makes auto-formatting nicer for build methods.
+    
+          floatingActionButton: FloatingActionButton(
+            onPressed: _getImage,
+            tooltip: 'Select',
+            child: Icon(Icons.add),
+          ), 
+        );
+        return scaffold;
   }
 }
